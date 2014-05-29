1986762a32201c37299ede4e4df2692d6f70491a
Ioannis Mitliagkas <ioannis@utexas.edu>
2014-05-29 00:35:42 -0500

    Added tpower implementation.

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 """The main execution file"""
 
-from numpy import random
 import numpy
+
 from spannogram import *
+from tpower import *
+
 
 __author__ = 'Ioannis Mitliagkas'
 __copyright__ = "Copyright 2014, Ioannis Mitliagkas"
-__credits__ = ["Dimitris Papailiopoulos"]
 __license__ = "MIT"
 __version__ = "0.0.1"
 __email__ = "ioannis@utexas.edu"
 __status__ = "alpha"
 
 
-p = 20
+p = 6
 n = 10000
-d = 3
+d = 2
 sigma = 0.01
 
 v = random.randn(p, 1)
@@ -37,11 +38,17 @@ if False:
     print
     print value
 else:
-    Vo = spcaold(X.dot(X.T), 2, 2, 2)
+    k=2
+    s=2
+    Vo = spcaold(X.dot(X.T), s, k, 2)
     print Vo.T
     print
-    V = spca(X.dot(X.T), 2, 2, 2)
+    V = spca(X.dot(X.T), s, k, 2)
     print V.T
+    print
+    Vt = tpower(X.dot(X.T), s, k)
+    print Vt.T
+    print
 
 
 
